d679b02742216b8e7b16db12a6fed686dc7d9459
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2022-06-07 21:40:21 +0530

    feat: nested callback & read file

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData = (callback) => {
+  let saveData = [];
+
+  fs.readFile(file1, (err, data1) => {
+    if (err) {
+      return callback(err);
+    } else {
+      saveData.push(JSON.parse(data1).message.split(" ")[1]);
+    }
+
+    fs.readFile(file2, (err, data2) => {
+      if (err) {
+        return callback(err);
+      } else {
+        saveData.push(JSON.parse(data2)[0].message.split(" ")[1]);
+      }
+
+      fs.readFile(file3, (err, data3) => {
+        if (err) {
+          return callback(err);
+        } else {
+          saveData.push(JSON.parse(data3)[0].data.message.split(" ")[1]);
+        }
+
+        callback(null, saveData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
